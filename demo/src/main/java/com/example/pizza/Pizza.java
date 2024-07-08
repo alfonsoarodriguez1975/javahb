e977ec6eec7f427da11112f14762dee3f2684bb8
alfonsoarodriguez1975 <alfonso.a.rodriguez@gmail.com>
2024-07-08 10:45:35 +0000

    Cambios en componentes

--- demo/src/main/java/com/example/pizza/Pizza.java
+++ demo/src/main/java/com/example/pizza/Pizza.java
@@ -1,9 +1,48 @@
 package com.example.pizza;
 
+import java.util.List;
+import java.util.UUID;
+
 public class Pizza extends EntityBase {
-    name,
-        description,
-        url,
-        precio -> read only -> sum(precio-ingredientes) * 1,2
-        ingredientes
+        private String Name;
+        private String Description;
+        private String Url;
+        private List<Ingredient> Ingredientes;
+
+        public String getName() {
+            return Name;
+        }
+        public String getDescription() {
+            return Description;
+        }
+        public String getUrl() {
+            return Url;
+        }
+
+        public Double getPrecio() {
+            Double precioIngredientes = 0.0;
+            for (Ingredient ingredient : Ingredientes) {
+                precioIngredientes += ingredient.getPrecio();
+            }
+            return precioIngredientes * 1.2;
+        }
+
+        public List<Ingredient> getIngredientes() {
+            return Ingredientes;
+        }
+
+        private Pizza(UUID id, String name, String description, String url, 
+                List<Ingredient> ingredientes) {
+            super(id);
+            Name = name;
+            Description = description;
+            Url = url;     
+            this.Ingredientes = ingredientes;
+        }
+
+        public static Pizza Create (UUID id, String name, String description, String url, 
+        List<Ingredient> ingredientes){
+            return new Pizza(id, name, description, url, ingredientes);
+        }      
+        
 }
